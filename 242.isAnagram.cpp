2e7e049681c97e396346ed0a6c4f160e5d5bec1b
alan <1413775276@qq.com>
2023-11-14 12:55:51 +0800

    2023.11.14

--- 242.isAnagram.cpp
+++ 242.isAnagram.cpp
@@ -1,5 +1,34 @@
 /*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+*/
+
+/*
+Solution
+1.Hash table
+    array set map
+2.元素比较少 用array
+3.创建一个vector包含26个元素 分别映射 26个字母
+4.分别遍历string s和string t
+    遍历s时 对应的下标值++ 比如
+                            ‘a' nums[0]++
+                            ’b‘ nums[1]++
+    遍历t时 对应下标值--
+5。遍历数组vector 如果nums[i] != 0 return false else return true;
 */
 #include <iostream>
 #include <string>
