2c409aae41ddcdbd762ef6906327a1ed61d1a0f6
alan <1413775276@qq.com>
2023-11-19 19:09:40 +0800

    2023.11.19

--- 206.reverseList.cpp
+++ 206.reverseList.cpp
@@ -8,9 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include <iostream> 
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
 
     }
 };
+
+int main()
+{
+    
+}
