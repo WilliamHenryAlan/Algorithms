24067c9cee77300b1749ef7d5839d415987df965
alan <1413775276@qq.com>
2023-11-28 11:52:37 +0800

    2023.11.28

--- BruteForceStringMatching.cpp
+++ BruteForceStringMatching.cpp
@@ -1,12 +1,20 @@
 /*
-朴素模式匹配字符串
-
+朴素模式匹配字符串 暴力破解
+1.两个指针i j分别指向主串和模式串 一般情况下主串长度大于模式串
+2.一共比较n-m+1次
+3.匹配条件为i<mainString.size() and j<subString.size() 
+        要遍历完才知道是否匹配 所以不能是i<mainString.size()-subString.size()+1 and j<subString.size()
+4.当匹配成功时 j == subString.size()
+5.如果单个char匹配成功 i++ j++
+  如果匹配过程中失败 i指针要进行回溯 i = i-j+1 ,j=0
+6.跳出循环后 如果匹配成功j == subString.size() return i-j为下标
+               匹配失败 return -1 
 */
 #include <iostream>
 #include <string>
 int bruteForceStringMatching(std::string& mainString,std::string& subString) {
     int i = 0,j = 0;
-    while (i < mainString.size()-subString.size()+1 and j < subString.size()) {
+    while (i < mainString.size() and j < subString.size()) {
         if (mainString[i] == subString[j]) {
             ++i;
             ++j;
@@ -22,8 +30,8 @@ int bruteForceStringMatching(std::string& mainString,std::string& subString) {
     }
 }
 int main() {
-    std::string mainString = "abaaabaabcaabcbab";
-    std::string subString = "abaabc";
+    std::string mainString = "mississippi";
+    std::string subString = "issi";
     int ret = bruteForceStringMatching(mainString,subString);
     std::cout << "return index = " << ret << std::endl;
 }
