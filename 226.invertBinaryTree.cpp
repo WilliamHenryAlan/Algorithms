e8cd7f3504999c40c1801ff12558910197fc7793
alan <1413775276@qq.com>
2023-12-04 15:47:12 +0800

    2023.12.4

--- 226.invertBinaryTree.cpp
+++ 226.invertBinaryTree.cpp
@@ -1,3 +1,23 @@
+
+/*
+给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+示例 2：
+
+输入：root = [2,1,3]
+输出：[2,3,1]
+
+示例 3：
+
+输入：root = []
+输出：[]
+
+
+*/
 #include<iostream>
 #include <vector>
 #include <queue>
@@ -19,5 +39,6 @@ public:
         std::swap(root->left,root->right);
         invertTree(root->left);
         invertTree(root->right);
+        return root;
     }
 };
