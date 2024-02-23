1ddc11fc8c1d77dd6ad12c5595d9c09e1d39727d
alan <1413775276@qq.com>
2024-02-23 15:47:48 +0800

    February 23.2024

--- cpp/3.lengthOfLongestSubstring.cpp
+++ cpp/3.lengthOfLongestSubstring.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
+
 /*
 滑动窗口
 1.遍历string 用hash存储每个char出现的次数
 2.每次记录最大的length 当出现重复元素时 移动left去重
 3.更新length = right - left + 1
-
 */
 class Solution {
 public:
@@ -26,5 +26,26 @@ public:
             ans = max(ans,r-l+1);       //uodate longest length
         }
         return ans;
+    }
+};
+/*
+review 2024.2.23
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> record;
+        int cnt = 0;
+        int ret = 0;
+        for (int j = 0,i = 0;j < s.size();j++) {
+            record[s[j]]++;
+            cnt++;
+            while (record[s[j]] > 1) {
+                record[s[i++]]--;
+                cnt--;
+            }
+            ret = max(cnt,ret);
+        }
+        return ret;
     }
 };
