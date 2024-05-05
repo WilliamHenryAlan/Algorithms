1dc1d67e2c4fd965078c72acce7b07b8e70b6a2a
alan <1413775276@qq.com>
2024-05-05 11:08:48 +0800

    May.5

--- Implementation/Vector.cpp
+++ Implementation/Vector.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using std::cout;
 using std::endl;
-
+struct Print {
+    void operator() (int elem) const {
+        std::cout << elem << " ";
+    }
+};
 int main() {
-    std::vector<int> arr{10,100,2,3};
+    std::vector<int> arr {10,100,2,3};
     for (int i = 0;i <= arr.size();i++) {
         cout << &arr[i] << " ";
     }
@@ -13,4 +18,8 @@ int main() {
     cout << "&*arr.end() = " << &*(arr.end()) << endl;
     cout << "&*arr.rbegin() = " << &*arr.rbegin() << endl;
     cout << "&*arr.rend() = " << &*(arr.rend()) << endl;
+    
+    std::for_each(arr.rbegin(),arr.rend(),Print()); //函数对象+反向迭代器+for_each实现逆向遍历
+
+    cout << endl;
 }
