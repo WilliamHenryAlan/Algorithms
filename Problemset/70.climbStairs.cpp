5d0a93930b5c70367ed5e36c09f5d6fb21f4d85b
alan <1413775276@qq.com>
2024-05-23 14:47:10 +0800

    2024.5.23

--- Problemset/70.climbStairs.cpp
+++ Problemset/70.climbStairs.cpp
@@ -22,3 +22,21 @@ public:
         return dp[n];
     }
 };
+
+/*
+review 2024.5.23
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        vector<int> dp(n);
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2;i < n;i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+};
--- Problemset/746.minCostClimbingStairs.cpp
+++ Problemset/746.minCostClimbingStairs.cpp
@@ -22,10 +22,26 @@ public:
     */
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        if (n < 2) return n;
+        if (n < 2) return 0;
         vector<int> dp(n+1,0);
         for (int i = 2;i <= n;i++) {
             dp[i] = std::min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[n];
+    }
+};
+
+/*
+review 2024.5.23
+*/
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if (cost.size() < 2) return 0;
+        int n = cost.size();
+        vector<int> dp(n + 1,0);
+        for (int i = 2;i <= n;i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1],dp[i - 2] + cost[i - 2]);
         }
         return dp[n];
     }
