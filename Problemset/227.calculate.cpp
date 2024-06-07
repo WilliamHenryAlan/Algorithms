10437a8c3deba786c419591b6e34741832e87649
alan <1413775276@qq.com>
2024-06-07 14:33:07 +0800

    2024.06.07

--- Problemset/227.calculate.cpp
+++ Problemset/227.calculate.cpp
@@ -6,57 +6,124 @@ using namespace std;
 224.基本计算器
 227.基本计算器II
 772.基本计算器III
-拆分问题；
-1.string => int
-2.处理 + -
-3.处理 * /
 */
+
+class Solution {
+public:
+    void removeSpace(string& str) {
+        str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    }
+
+    void calcu() {
+        if (nums.size() < 2 || ops.empty()) return;
+        int b = nums.top(); nums.pop();
+        int a = nums.top(); nums.pop();
+        char op = ops.top(); ops.pop();
+        int result = 0;
+        if (op == '+') {
+            result = a + b;
+        } else if (op == '-') {
+            result = a - b;
+        }
+        nums.push(result);
+    }
+
+    bool checkDigit(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
+    int calculate(string s) {
+        // 避免开头出现'-'
+        nums.push(0);
+        // 去除所有' '
+        removeSpace(s);
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '(') {
+                ops.push(ch);
+            } else if (ch == ')') {
+                // 直到遇到'('
+                while (!ops.empty()) {
+                    char op = ops.top();
+                    if (op != '(') {
+                        calcu();
+                    } else {
+                        // 遇到‘（’ pop并break
+                        ops.pop();
+                        break;
+                    }
+                }
+            } else {
+                if (checkDigit(ch)) {
+                    int sum = 0;
+                    int j = i;
+                    while (j < n && checkDigit(s[j])) {
+                        sum = sum * 10 + (s[j++] - '0');
+                    }
+                    nums.push(sum);
+                    i = j - 1;
+                } else {
+                    // 处理操作符
+                    if (i > 0 && (s[i - 1] == '(' || s[i - 1] == '+' || s[i - 1] == '-')) {
+                        nums.push(0);
+                    }
+                    // 确保ops栈中优先级低于当前运算符的运算都执行完
+                    while (!ops.empty() && ops.top() != '(') {
+                        calcu();
+                    }
+                    ops.push(ch);
+                }
+            }
+        }
+        // 最后计算剩余的运算
+        while (!ops.empty()) {
+            calcu();
+        }
+        return nums.top();
+    }
+
+private:
+    stack<int> nums;  // 存储number
+    stack<char> ops;  // 存储operator
+};
+
+
+
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> stk;
-        int len = s.size();
-        char sign = '+';
-        int num = 0;
-        for (int i = 0;i < len;++i) {
-            char c = s[i];
-            //遇到数字 先进行累加
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            }
-            /*
-            1.不能为数字 而且不能为空格
-            2.当遍历到以后一个数字时 也要进行计算 如果没有i == len-1这个条件
-            那么就会进入上一个if 最后一个数字的运算会被留在num中
-            */
-            if ((!isdigit(c) && c != ' ') || i == len-1) {
-                int temp = 0;
-                switch (sign) {
-                    case '+':
-                        stk.push(num);break;
-                    case '-':
-                        stk.push(-num);break;
-                    case '*':
-                        temp = stk.top();
-                        stk.pop();
-                        stk.push(temp*num);
-                        break;
-                    case '/':
-                        temp = stk.top();
-                        stk.pop();
-                        stk.push(temp/num);
-                        break;
+        stack<int> ops;
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s[i] == ' ') {
+                i++;
+            } else if (s[i] == '+') {
+                sign = ops.top();
+                i++;
+            } else if (s[i] == '-') {
+                sign = -ops.top();
+                i++;
+            } else if (s[i] == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s[i] == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + s[i] - '0';
+                    i++;
                 }
-                sign = c;
-                num = 0;
+                ret += sign * num;
             }
         }
-        //把栈中的数值进行累加
-        int ans = 0;
-        while (!stk.empty()) {
-            ans += stk.top();
-            stk.pop();
-        }
-        return ans;
+        return ret;
     }
 };
