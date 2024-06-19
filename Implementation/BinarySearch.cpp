850b9a1255765b46cbb5eda23dd627b5385cc3e9
alan <1413775276@qq.com>
2024-06-19 13:43:25 +0800

    2024.06.19

--- Implementation/BinarySearch.cpp
+++ Implementation/BinarySearch.cpp
@@ -5,12 +5,20 @@
 时间复杂度：n个数 n, n/2, n/4, n/8...n/2^k => O(logN)
 局限性：1.需要有序 但是无序情况下也可以用二分找区间
        2.需要能够随机访问 链式存储没可以用跳表
+
+我们不关心区间内数组的情况 而是可以确定区间外数组的情况
+
+>= x 用lower_bound解决
+> >=x+1
+< (>=x) - 1
+<= (>x) - 1
+
 */
 #include <iostream>
 #include <vector>
 using namespace std;
 
-//找到 >= target的最小值
+void buildArr(std::vector<int>& vi);
 int lower_bound1(const vector<int>& nums,int target);
 //找到 >= target的最小值
 int lower_bound2(const std::vector<int>& nums, int target);
@@ -21,39 +29,29 @@ int binarySearch(const std::vector<int>& vi,const int& target);
 void print(const std::vector<int>& vi);
 
 int main() {
-    std::vector<int> array;
-    array.push_back(1);
-    array.push_back(2);
-    array.push_back(2);
-    array.push_back(2);
-    array.push_back(4);
-    array.push_back(4);
-    array.push_back(6);
-    array.push_back(7);
-    print(array);
-    cout << "func lower_bound() = " << distance(array.begin(),lower_bound(array.begin(),array.end(),3)) << endl;
-    cout << "func lower_bound() = " << lower_bound2(array,3) << endl;
-    cout << "func lower_bound() = " << distance(array.begin(),upper_bound(array.begin(),array.end(),5)) << endl;
-    cout << "func lower_bound() = " << upper_bound1(array,5) << endl;
-
+    std::vector<int> array;buildArr(array);print(array);
+    cout << "std::lower_bound = " << distance(array.begin(),lower_bound(array.begin(),array.end(),3)) << endl;
+    cout << "func lower_bound1 = " << lower_bound1(array,3) << endl;
+    cout << "std::upper_bound = " << distance(array.begin(),upper_bound(array.begin(),array.end(),5)) << endl;
+    cout << "func lower_bound2 = " << lower_bound2(array,6) << endl; //lower_bound <=> upper_bound
 }
-/*
-    我们不关心区间内数组的情况 而是可以确定区间外数组的情况
-*/
+
+
+//[]
 int lower_bound1(const vector<int>& nums,int target) {    
-    int l = 0,r = nums.size();//左闭右闭区间   循环条件要写为l <= r因为l = r的时候是有意义的
+    int l = 0,r = nums.size();// 循环条件要写为l <= r因为l = r的时候是有意义的
     while (l <= r) { //循环结束之后 r一定在l的左边
         int m = l + (r - l) / 2;//防止溢出
-        if (nums[m] < target) l = m + 1;//l左边的数组严格小于target
-        else r = m - 1; //右边的数组严格大于等于target
+        if (nums[m] < target) l = m + 1;//R + 1的区域 >= target
+        else r = m - 1; //L - 1的区域 < target
     }
     return l;   //r + 1
 }
-//找到 >= target的最小值
+ //[)
 int lower_bound2(const std::vector<int>& nums, int target) {
     if (nums.size() == 0) return -1;
     int len = nums.size();
-    int l = 0,r = len;  //左闭右开区间
+    int l = 0,r = len;
     while (l < r) {     //循环退出的时候left = right
         int mid = l + (r - l) / 2;  //防止溢出
         if (nums[mid] >= target) {  //说明[mid,right)严格大于等于target
@@ -116,47 +114,17 @@ void print(const std::vector<int>& vi) {
     }
     std::cout << std::endl;
 }
-
-
-/*
-二分查找也叫折半查找 O(logn)
-前提是必须是有序序列
-
-solution:
-以升序为例
-双指针 low high 分别指向头尾的index
-遍历整个array 每次求middle index = (low+high)/2
-对比target和array[mid]: 会出现三种情况
-if (array[mid] > target)  mid的值比目标大 升序序列 说明目标在左边   移动high = mid-1
-if (array[mid] < target)  mid的值比目标小 升序序列 说明目标在右边   移动low = mid+1
-if (array[mid] == target) 找到target    return mid    
-
-迭代array的条件为low <= heigh
-选取边界条件 当low == heigh 时 应该继续循环 这样才能判定是否存在target
-
-target = 2
-arr:    1   2   4   6   7
-        ^       ^       ^
-        low     mid    high
-arr:    1   2   4   6   7
-        ^   ^   ^       
-        l   h   m 
-arr:    1   2   4   6   7
-        ^   ^          
-        l   h   
-        ^
-        m
-arr:    1   2   4   6   7
-        ^   ^          
-        l   h   
-        ^
-        m
-arr:    1   2   4   6   7
-            ^          
-            h   
-            ^
-            l
-边界情况low == heigh 应该继续 mid = 1 array[1] == target return 1
-        
-*/
-
+void buildArr(std::vector<int>& vi) {
+    array.push_back(1);
+    array.push_back(2);
+    array.push_back(4);
+    array.push_back(6);
+    array.push_back(7);
+    array.push_back(2);
+    array.push_back(2);
+    array.push_back(4);
+    array.push_back(4);
+    array.push_back(6);
+    array.push_back(7);
+}
+    
