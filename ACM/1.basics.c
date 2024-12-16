cf3c3f815e91757896d436967b1c288fb81c4b49
alan <1413775276@qq.com>
2024-12-16 11:43:04 +0800

    2024.12.16

--- ACM/1.basics.c
+++ ACM/1.basics.c
@@ -7,17 +7,10 @@ while (scanf("%d %d") != EOF) { //EOF是预定义量 = -1
 	...
 }
 //scanf返回读取数据个数 所以也可以写成
-while (scanf("%d %d") != 2) { //EOF是预定义量 = -1 
+while (scanf("%d %d") != 2) { 
 	...
 }
 // C++ language
 while (cin >> a >> b) {
 	...
-}
-
-
-#include <stdio.h>
-int main() {
-
-	return 0;
 }
